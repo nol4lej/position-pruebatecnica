2fcb8b7e5f0c70c9167f27f50dee8fb7e207683e
nol4lej <nome.ahumada@outlook.com>
2024-06-27 11:20:35 -0400

    Agregado beforeEach para manejar rutas privadas en Vue Router

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -1,9 +1,12 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 import routes from "./routes.list";
+import { beforeEachRoute } from "./router.middleware.js";
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes
 });
+
+router.beforeEach(beforeEachRoute);
 
 export { router };
