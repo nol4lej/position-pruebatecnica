42101202bc8524dbdf1179282ebe41cb9ffd84e8
nol4lej <nome.ahumada@outlook.com>
2024-06-27 11:38:58 -0400

    Alias '@services' agregado para rutas './resources/js/services'

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,16 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true
         }),
-    vue()
-  ],
+        vue()
+    ],
     resolve: {
         alias: {
             vue: "vue/dist/vue.esm-bundler.js",
             '@router': path.resolve(__dirname, './resources/js/router'),
             '@components': path.resolve(__dirname, './resources/js/components'),
-            '@views': path.resolve(__dirname, './resources/js/views')
+            '@views': path.resolve(__dirname, './resources/js/views'),
+            '@config': path.resolve(__dirname, './resources/js/config'),
+            '@services': path.resolve(__dirname, './resources/js/services'),
         }
     }
 });
