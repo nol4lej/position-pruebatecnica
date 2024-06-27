ee1e7c3854491d58a6055d8cc096d7088b71ef62
nol4lej <nome.ahumada@outlook.com>
2024-06-27 00:33:52 -0400

    feat: definir rutas de autenticación en routes/api.php
    - Se establecieron rutas bajo el prefijo 'auth' para gestionar registro, inicio de sesión y cierre de sesión.
    - Las rutas '/register' y '/login' apuntan a los métodos correspondientes del controlador AuthController.
    - La ruta '/logout' utiliza el middleware 'auth:sanctum' para asegurar que solo usuarios autenticados puedan cerrar sesión

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("auth")->group(function () {
-    Route::get("/", function () {
-        return "hola";
-    })->name("");
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
 // Route::get('/user', function (Request $request) {
