b9e4723522ed84230834907a7833315d1e8066c5
nol4lej <nome.ahumada@outlook.com>
2024-06-27 10:01:54 -0400

    Agregar alias para rutas en configuración de Vite
    - Configurar alias para router, componentes y vistas

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true
         }),
-    ],
+    vue()
+  ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+            '@router': path.resolve(__dirname, './resources/js/router'),
+            '@components': path.resolve(__dirname, './resources/js/components'),
+            '@views': path.resolve(__dirname, './resources/js/views')
+        }
+    }
 });
