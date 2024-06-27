b6a5f1c3d74ab1152065074725818e3a19a859bc
nol4lej <nome.ahumada@outlook.com>
2024-06-27 02:28:41 -0400

    Configuración de rutas para transacciones:
    - Se añadieron las rutas para manejar las operaciones CRUD de transacciones bajo el middleware 'auth:sanctum' para asegurar que solo usuarios autenticados puedan acceder a ellas.
    - Se definieron las siguientes rutas:
      - POST '/transaction': Para almacenar una nueva transacción.
      - GET '/transaction': Para obtener todas las transacciones del usuario actual.
      - PUT '/transaction/{id}': Para actualizar una transacción existente por su ID.
      - POST '/transaction/{id}': Para cambiar el estado de una transacción por su ID.
    - Se agruparon las rutas relacionadas con autenticación bajo el prefijo '/auth', incluyendo registro, inicio de sesión y cierre de sesión.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,14 @@ Route::prefix('auth')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/transaction', [TransactionController::class, 'store']);
+    Route::get('/transaction', [TransactionController::class, 'index']);
+    Route::put('/transaction/{id}', [TransactionController::class, 'update']);
+    Route::post('/transaction/{id}', [TransactionController::class, 'changeStatus']);
+});
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
-// })->middleware('auth:sanctum');
+// });
