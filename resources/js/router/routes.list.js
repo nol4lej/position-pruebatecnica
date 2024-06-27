c3b68f88cb7549dba0a518a43ab33478d9494bd5
nol4lej <nome.ahumada@outlook.com>
2024-06-27 11:24:19 -0400

    Agregadas rutas privadas que requieren autenticación y layout principal
    Se añadieron rutas al sistema de enrutamiento de Vue que están protegidas y requieren autenticación para acceder. El layout principal, representado por 'MainLayout.vue', actúa como contenedor para las rutas protegidas. Las rutas incluidas son:
    - '/login': Ruta de acceso para iniciar sesión.
    - '/': Ruta principal protegida que redirige al componente 'HomeView.vue' dentro del layout principal ('MainLayout.vue'). Esta ruta requiere autenticación, controlada mediante la metaetiqueta 'requiresAuth' en Vue Router.

--- resources/js/router/routes.list.js
+++ resources/js/router/routes.list.js
@@ -1,7 +1,19 @@
+import MainLayout from "@views/MainLayout.vue";
+import LoginView from "@views/LoginView.vue";
 import HomeView from "@views/HomeView.vue";
 
 const routes = [
-  { path: "/", component: HomeView }
+  { path: "/login", name: 'login', component: LoginView },
+  {
+    path: '/',
+    component: MainLayout,
+    meta: { requiresAuth: true },
+    children: [
+      { path: '', name: 'default', redirect: 'home' }, // Redirecciona a una subruta por defecto
+      { path: 'home', name: 'home', component: HomeView },
+    ]
+  }
+
 ];
 
 export default routes;
