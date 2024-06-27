2b93105e0521b5ac56400a1fe51f6b626f7023fb
nol4lej <nome.ahumada@outlook.com>
2024-06-27 11:23:12 -0400

    Agregado middleware para manejar autenticación de rutas
    Se implementó un middleware en Vue Router para gestionar la autenticación de las rutas. El middleware verifica si la ruta requiere autenticación basándose en la configuración de 'meta.requiresAuth'. Si el usuario está autenticado (verificado mediante la existencia de un token válido en localStorage), permite la navegación a la ruta solicitada. En caso contrario, redirige al usuario a la ruta '/login'.

---
+++ resources/js/router/router.middleware.js
@@ -0,0 +1,18 @@
+function isAuthenticated() {
+  const token = localStorage.getItem('token');
+  return !!token; // Devuelve true si el token existe y es válido, de lo contrario false
+}
+
+export const beforeEachRoute = (to, from, next) => {
+  console.log(to);
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+
+    if (isAuthenticated()) {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    next(); // La ruta no requiere autenticación, permite continuar
+  }
+}
