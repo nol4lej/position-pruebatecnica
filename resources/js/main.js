9c6b46d7654ef225d31f6e38e2b8819633e48089
nol4lej <nome.ahumada@outlook.com>
2024-06-27 11:37:52 -0400

    vuetify agregado a app

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,10 @@
-import "./bootstrap.js";
+import "@config/bootstrap.js";
+import vuetify from "@config/vuetify";
 import { createApp } from "vue";
 import { router } from "@router";
 import App from "./app.vue";
 
 createApp(App)
+.use(vuetify)
 .use(router)
 .mount("#root")
